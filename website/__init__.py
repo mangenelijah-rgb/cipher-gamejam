08c0a8e5cc9f04ff8edc06df0968810ee561d748
mangenelijah-rgb <mangenelijah@gmail.com>
2025-11-14 07:55:02 -0500

    Resolved port between flask app and docker.

--- website/__init__.py
+++ website/__init__.py
@@ -3,4 +3,9 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'fgh 7^& HJ6"GHJ)@SDFCV  fgrtcfGC65vb__%l'
+
+    from .views import views
+
+    app.register_blueprint(views,url_prefix="/")
+
     return app
